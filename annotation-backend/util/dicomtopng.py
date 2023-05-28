7334415d7f4b25c991bf010078e1e63177afa8b1
Nazli As <f.nazlias@gmail.com>
2023-05-28 19:28:58 +0300

    dicom to png format

--- annotation-backend/util/dicomtopng.py
+++ annotation-backend/util/dicomtopng.py
@@ -4,7 +4,36 @@ import pandas as pd
 import pymongo
 from pydicom import dcmread
 from PIL import Image
+import pylibjpeg
 import csv
+
+from flask import Flask, jsonify, request, Response
+from pymongo import MongoClient
+from waitress import serve
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+cors = CORS(app)
+
+client = MongoClient("mongodb://localhost:27017")
+
+cxr_db = client['cxr']  # cxr database
+
+png_xray_images=cxr_db.png_xray_images
+
+def find_classes():
+    csv_file = r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\util\train.csv"
+    dicom_folder_path=r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\dicomToPng\dicomImages\New"
+    for filename in os.listdir(dicom_folder_path):
+        dicom_file_path = os.path.join(dicom_folder_path, filename)
+        name = os.path.splitext(filename)[0]
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row['class_id'])
+
 
 def find_information(value):
     csv_file=r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\util\train.csv"
@@ -21,7 +50,7 @@ def find_information(value):
 
 
 def convert_dicom_to_jpg():
-    dicom_folder_path=r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\dicomToPng\dicomImages"
+    dicom_folder_path=r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\dicomToPng\dicomImages\New"
     output_folder_path=r"C:\Users\Naz\PycharmProjects\annotating-chest-xray-images\annotation-backend\dicomToPng\pngImages"
 
     image_information=[]
@@ -49,13 +78,12 @@ def convert_dicom_to_jpg():
                 "image":encoded_image,
                 "data":data
             }
+            png_xray_images.insert_one(image_dict)
+            print()
             image_information.append(image_dict)
-
-
-    for i in image_information:
-        print(i["filename"])
 
     return image_information
 
-convert_dicom_to_jpg()
 
+
+convert_dicom_to_jpg()
